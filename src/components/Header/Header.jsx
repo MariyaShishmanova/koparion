de8ca2e2043bd302d1acc0d38bbdd1e51fb43ad4
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-07-19 10:36:51 +0300

    fix: header menu functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import CurrencySelect from './CurrencySelect';
 import AccountArea from './AccountArea';
 import SearchForm from './SearchForm';
@@ -5,8 +7,39 @@ import Menu from './Menu';
 import Cart from './Cart';
 
 export default function Header() {
+  const headerRef = useRef(null);
+
+  // Function to set CSS properties
+  function setCssProperty(property, value, unit = 'px') {
+    if (!property || value === undefined) {
+      throw new Error('Both property and value are required');
+    }
+    document.documentElement.style.setProperty(property, `${value}${unit}`);
+  }
+
+  // Function to get and update the header height
+  function handleHeights() {
+    if (headerRef.current) {
+      const headerHeight = headerRef.current.offsetHeight;
+      setCssProperty('--header-height', headerHeight);
+    }
+    setCssProperty('--viewport-height', window.innerHeight);
+  }
+
+  // Set initial viewport height and header height
+  useEffect(() => {
+    handleHeights();
+
+    // update on window resize
+    window.addEventListener('resize', handleHeights);
+
+    return () => {
+      window.removeEventListener('resize', handleHeights);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <div className="container">
         <div className="header__inner">
           <div className="header__bar">
